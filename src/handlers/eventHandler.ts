6bd20d17acce30fe119d2fee78a48771dd819a94
Kauã Felipe <kauafaguiari@gmail.com>
2025-03-06 19:56:08 -0300

    fix(events): path to events dir

--- src/handlers/eventHandler.ts
+++ src/handlers/eventHandler.ts
@@ -1,13 +1,21 @@
 import { Client } from "discord.js";
-import { readdirSync } from "fs";
+import { existsSync, readdirSync } from "fs";
 import { join } from "path";
 
 export function loadEvents(client: Client) {
     const eventsPath = join(__dirname, "events");
-    const eventFiles = readdirSync(eventsPath).filter(file => file.endsWith(".ts") || file.endsWith(".js"));
 
+    console.log(`📂 Tentando carregar eventos de: ${eventsPath}`);
+    
+    if (!existsSync(eventsPath)) {
+        console.error(`🚨 O diretório ${eventsPath} não existe ou está inacessível!`);
+    } else {
+        const eventFiles = readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+        console.log(`🔍 Encontrados ${eventFiles.length} arquivos de eventos:`, eventFiles);
+    
     for (const file of eventFiles) {
-        const event = require(join(eventsPath, file));
+        const event = require(join(__dirname, "events", file));
+
         if (!event.name || !event.execute) {
             console.warn(`⚠️ O arquivo ${file} não possui um nome ou função execute.`);
             continue;
@@ -19,4 +27,5 @@ export function loadEvents(client: Client) {
             client.on(event.name, (...args) => event.execute(...args));
         }
     }
+}
 }
